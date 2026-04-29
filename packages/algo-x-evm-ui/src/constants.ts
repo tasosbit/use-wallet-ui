b7c41ee1034937905a6340093369d0b2e4214cd1
Bit D13 <bit@d13.co>
2026-04-29 16:56:22 +0200

    chore: update xchain domain to .co - 0.0.16

--- packages/algo-x-evm-ui/src/constants.ts
+++ packages/algo-x-evm-ui/src/constants.ts
@@ -1,1 +1,1 @@
-export const DOCS_URL = (import.meta.env.VITE_XCHAIN_DOCS_URL ?? 'https://xchain.algorand.tech/docs').replace(/\/+$/, '')
+export const DOCS_URL = (import.meta.env.VITE_XCHAIN_DOCS_URL ?? 'https://xchain.algorand.co/docs').replace(/\/+$/, '')
